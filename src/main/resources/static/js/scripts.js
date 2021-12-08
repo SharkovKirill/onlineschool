ab709b4dda02aed5be0680b969323856bd09f79d
AntonKAb <anton_10_1999@mail.ru>
2021-12-09 04:14:47 +0700

    update course

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#add__course').click(function () {
+        $('.modal').addClass('modal_active');
+    });
+
+    $('.modal__close-button').click(function () {
+        $('.modal').removeClass('modal_active');
+    });
+
+    $('.modal').mouseup(function (e) {
+        let modalContent = $(".modal__content");
+        if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+            $(this).removeClass('modal_active');
+        }
+    });
+});
